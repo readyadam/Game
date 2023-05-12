b9ba4bfdb5a7ed03472547e07ab46b0c19ee9794
readyadam <133392811+readyadam@users.noreply.github.com>
2023-05-12 19:15:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+    // Itt írhatjuk a betöltődéskor végrehajtandó utasításokat
+  });
+  
+  let myButton = document.querySelector('#my-button');
+  myButton.addEventListener('click', function() {
+    // Itt írhatjuk a gombra kattintáskor végrehajtandó utasításokat
+  });
+  
+    
